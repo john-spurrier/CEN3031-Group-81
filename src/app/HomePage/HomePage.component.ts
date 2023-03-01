c690286da470e80c7a101a31cafaeab219faeacd
john-spurrier <92879756+john-spurrier@users.noreply.github.com>
2023-03-01 18:57:13 -0500

    Add files via upload
    changes to homepage

--- src/app/HomePage/HomePage.component.ts
+++ src/app/HomePage/HomePage.component.ts
@@ -1,24 +1,30 @@
 import { Component } from '@angular/core';
 
+@Component({
+  selector: 'app-root',
+  templateUrl: './HomePage.component.html',
+  styleUrls: ['./HomePage.component.css']
+})
+
 export class HomePageComponent {
-    constructor(formId: string) {
-      const form = document.getElementById(formId) as HTMLFormElement;
-      form.addEventListener("submit", this.submitForm.bind(this));
-    }
-  
+    //constructor(private formId : string) {
+      //const form = document.getElementById(formId) as HTMLFormElement;
+      //form.addEventListener("submit", this.submitForm.bind(this));
+    //}
+
     private async submitForm(event: Event) {
       event.preventDefault();
-  
+
       const form = event.target as HTMLFormElement;
       const name = form.elements.namedItem("name") as HTMLInputElement;
       const email = form.elements.namedItem("email") as HTMLInputElement;
       const message = form.elements.namedItem("message") as HTMLTextAreaElement;
-  
+
       if (!name.value || !email.value || !message.value) {
         alert("Please fill out all fields.");
         return;
       }
-  
+
       try {
         const response = await fetch("https://jsonplaceholder.typicode.com/posts", {
           method: "POST",
@@ -31,7 +37,7 @@ export class HomePageComponent {
             message: message.value
           })
         });
-  
+
         if (response.ok) {
           form.reset();
           alert("Form submitted successfully!");
@@ -44,8 +50,7 @@ export class HomePageComponent {
       }
     }
   }
-  
+
   document.addEventListener("DOMContentLoaded", () => {
-    const form = new HomePageComponent("contact-form");
+    const form = new HomePageComponent();
   });
-  
