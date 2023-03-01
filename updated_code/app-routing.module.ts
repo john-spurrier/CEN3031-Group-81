76b45de1bfe390da6539548ce9d5889920cdedf6
MattLee91 <110436723+MattLee91@users.noreply.github.com>
2023-03-01 17:21:27 -0500

    added 5 updated files

---
+++ updated_code/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './HomePage.component';
+import {LoginComponent} from "./login.component";
+import {AppComponent} from "./app.component";
+
+const routes: Routes = [
+{ path: '', component: HomePageComponent },
+  {path: 'login', component: LoginComponent }];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ updated_code/login.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html'
+})
+export class LoginComponent {
+  loginForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+
+  onSubmit() {
+    // handle form submission here
+    console.log(this.loginForm.value);
+  }
+}
