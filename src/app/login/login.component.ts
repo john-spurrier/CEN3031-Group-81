e7a70404b780b2f1d56992be35e5af2cb82b9ce3
maxfennessy <92819224+maxfennessy@users.noreply.github.com>
2023-03-01 17:11:27 -0500

    login files

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,31 +1,16 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
-import { AuthService } from '../auth.service';
-
 
 @Component({
   selector: 'app-login',
-  template: 
-  `<h1>Login Page</h1>
-    <form [formGroup]="loginForm" (ngSubmit)="onSubmit()">
-      <div>
-        <label for="username">Username:</label>
-        <input type="text" id="username" formControlName="username">
-      </div>
-      <div>
-        <label for="password">Password:</label>
-        <input type="password" id="password" formControlName="password">
-      </div>
-      <button type="submit" [disabled]="!loginForm.valid">Login</button>
-    </form>
-    <button (click)="goToOtherPage()">Home Page</button>
-  `,
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
 })
+
 export class LoginComponent {
   loginForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder, private router: Router, private auth: AuthService) {
+  constructor(private formBuilder: FormBuilder) {
     this.loginForm = this.formBuilder.group({
       username: ['', Validators.required],
       password: ['', Validators.required],
@@ -35,15 +20,5 @@ export class LoginComponent {
   onSubmit() {
     // handle form submission here
     console.log(this.loginForm.value);
-    const { username, password } = this.loginForm.value;
-    this.auth.login(username, password).subscribe(
-      () => console.log('Login successful'),
-      error => console.error(error)
-    );
   }
-
-  goToOtherPage() {
-    this.router.navigateByUrl('https://www.youtube.com/');
-  }
-
 }
