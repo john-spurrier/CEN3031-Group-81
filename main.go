0ed3505621aff2aa0fc4bcb0263d8989c043daaa
CHarmUF <93229700+CHarmUF@users.noreply.github.com>
2023-03-01 20:52:26 -0500

    GORM API Push
    These files encompass an implementation of the backend API using gorilla mux for http request routing and GORM as the Object Relational Model for the sqlite database.

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func helloWorld(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World")
+}
+
+func handleRequests() {
+	myRouter := mux.NewRouter().StrictSlash(true)
+	myRouter.HandleFunc("/", helloWorld).Methods("GET")
+	myRouter.HandleFunc("/users", AllUsers).Methods("GET")
+	myRouter.HandleFunc("/user/{name}/{email}", NewUser).Methods("POST")
+	myRouter.HandleFunc("/login", loginHandler).Methods("POST")
+	myRouter.HandleFunc("/user/{name}", DeleteUser).Methods("DELETE")
+	myRouter.HandleFunc("/user/{name}/{email}", UpdateUser).Methods("PUT")
+	log.Fatal(http.ListenAndServe(":8080", myRouter))
+}
+
+func main() {
+	fmt.Println("Go ORM Tutorial")
+
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	db.AutoMigrate(&User{})
+
+	user := User{Username: "janedoe", Password: "pass123"}
+	result := db.Create(&user)
+	if result.Error != nil {
+		panic(result.Error)
+	}
+
+	fmt.Printf("New user created with ID %d\n", user.ID)
+
+	handleRequests()
+}
---
+++ user.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB
+var err error
+
+type User struct {
+	gorm.Model
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+func AllUsers(w http.ResponseWriter, r *http.Request) {
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	var users []User
+	db.Find(&users)
+	json.NewEncoder(w).Encode(users)
+}
+
+func NewUser(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "New User Endpoint hit")
+}
+
+func DeleteUser(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Delete User Endpoint Hit")
+}
+
+func UpdateUser(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Update User Endpoint Hit")
+}
+
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	// Parse the request body to get the username and password
+	decoder := json.NewDecoder(r.Body)
+	var credentials struct {
+		Username string `json:"username"`
+		Password string `json:"password"`
+	}
+	err := decoder.Decode(&credentials)
+	if err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	// Check the credentials against the database
+	// ...
+	var user User
+	var validCredentials bool
+	result := db.Where("username = ? AND password = ?", "janedoe", "pass123").First(&user)
+	if result.Error != nil {
+		panic(result.Error)
+	}
+	if result.RowsAffected == 0 {
+		// User not found or password incorrect
+		validCredentials = false
+	} else {
+		// User found and password correct
+		validCredentials = true
+	}
+	// Return a response indicating success or failure
+	if validCredentials {
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprint(w, "Login successful")
+	} else {
+		w.WriteHeader(http.StatusUnauthorized)
+		fmt.Fprint(w, "Invalid username or password")
+	}
+}
