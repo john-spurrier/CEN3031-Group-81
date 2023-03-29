0fae1e3aab86e18d4487fd5ccdfac5d507bffa76
CHarmUF <93229700+CHarmUF@users.noreply.github.com>
2023-03-29 16:47:11 -0400

    Update Login Component
    Added HTTP Queries to Backend with regards to login credentials

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,31 +1,41 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule} from '@angular/forms';
+import {Router, ActivatedRoute} from "@angular/router";
 import { HttpClient } from '@angular/common/http';
+//import {AccountService} from "./account.service";
+import {first} from "rxjs";
+
 
 @Component({
   selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['login.component.css']
+  templateUrl: './login.component.html' ,
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  loginForm: FormGroup;
+  loginForm!:FormGroup;
+  loading = false;
+  submitted = false;
+  //loginForm: FormGroup;
+  constructor(private formBuilder: FormBuilder, private router: Router, private route: ActivatedRoute, private http: HttpClient) {}
 
-  constructor(private formBuilder: FormBuilder, private http : HttpClient) {
+  ngOnInit() {
     this.loginForm = this.formBuilder.group({
       username: ['', Validators.required],
-      password: ['', Validators.required],
+      password: ['', Validators.required]
     });
   }
 
   onSubmit() {
     // handle form submission here
     console.log(this.loginForm.value);
-    this.http.post('http://localhost:8080/user/{name}/{email}', this.loginForm.value).subscribe(
+    this.http.post('http://localhost:8080/login', this.loginForm.value).subscribe(
     response => {
        console.log('Post request successful:', response);
+       alert("Login successful, procceding to next page.");
     },
     error => {
-      console.error('Post request failed:', error);
+      console.error('Unauthorized User', error); 
+      alert("Login failed. Please check your username and password and try again.");
     }
 );
   }
