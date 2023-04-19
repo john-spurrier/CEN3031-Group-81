d943b0c9073e390d3a3eed4abf35937c4622d252
john-spurrier <92879756+john-spurrier@users.noreply.github.com>
2023-04-19 18:56:52 -0400

    Add files via upload

--- Sprint_4_Code/app-routing.module.ts
+++ Sprint_4_Code/app-routing.module.ts
@@ -5,27 +5,28 @@ import {LoginComponent} from "./login/login.component";
 import {AppComponent} from "./app.component";
 import {ThanksComponent} from "./thanks/thanks.component";
 import { PremiumPackageComponent } from './premium-package/premium-package.component';
+import { RegistrationComponent } from './registration/registration.component';
 import {StarterPackageComponent} from "./starter-package/starter-package.component";
-import {ShopComponent} from "./shop/shop.component";
-import {IntermedPackageComponent} from "./intermed-package/intermed-package.component";
-import {CartComponent} from "./cart/cart.component";
-import {RegistrationComponent} from "./registration/registration.component";
-import {CreditPageComponent} from "./credit-page/credit-page.component";
-import {MyPackageComponent} from "./my-package/my-package.component";
-import {PackageBoughtComponent} from "./package-bought/package-bought.component";
+import { ShopComponent } from './shop/shop.component';
+import { CreditPageComponent } from './credit-page/credit-page.component';
+import { CartComponent } from './cart/cart.component';
+import { IntermedPackageComponent } from './intermed-package/intermed-package.component';
+import { LoginGuard } from './login.guard';
+import { MyPackageComponent } from './my-package/my-package.component';
+import { PackageBoughtComponent } from './package-bought/package-bought.component';
 
 const routes: Routes = [
-{ path: '', component: HomePageComponent },
+  {path: '', component: HomePageComponent},
   {path: 'login', component: LoginComponent },
   {path: 'thanks', component: ThanksComponent},
   {path: 'premium-package', component: PremiumPackageComponent },
-  {path: 'starter-package', component: StarterPackageComponent },
-  {path: 'intermediate-package', component: IntermedPackageComponent},
-  {path: 'shop', component: ShopComponent},
+  {path: 'register', component: RegistrationComponent},
+  {path: 'starter-package', component: StarterPackageComponent  },
+  {path: 'intermediate-package', component: IntermedPackageComponent },
+  {path: 'shop', component: ShopComponent, canActivate: [LoginGuard]},
   {path: 'cart', component: CartComponent},
-  {path: 'registration', component: RegistrationComponent},
   {path: 'credit', component: CreditPageComponent},
-  {path: 'package', component: MyPackageComponent},
+  {path: 'package', component: MyPackageComponent, canActivate: [LoginGuard]},
   {path: 'package-bought', component: PackageBoughtComponent}];
 
 @NgModule({
