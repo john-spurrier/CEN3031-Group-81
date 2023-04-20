65e48a31ee6b7e68ff5c4641b651aa513bd79bc8
CHarmUF <93229700+CHarmUF@users.noreply.github.com>
2023-04-19 20:41:55 -0400

    Update package-bought.component.ts
    Added Backend Connection to Database that store the state of user purchases/transactions history of all three packages (starter, intermediate, and premium)

--- Sprint_4_Code/package-bought/package-bought.component.ts
+++ Sprint_4_Code/package-bought/package-bought.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { PackageService } from "../package.service";
 import {CartService} from "../cart.service";
+import { HttpClient } from '@angular/common/http';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-package-bought',
@@ -8,10 +10,23 @@ import {CartService} from "../cart.service";
   styleUrls: ['./package-bought.component.css']
 })
 export class PackageBoughtComponent {
-  constructor(private cartService: CartService, private packageService: PackageService) {}
+  constructor(private cartService: CartService, private packageService: PackageService, private http: HttpClient, private authServ: AuthService) {}
 
   ngOnInit()
   {
+    console.log('isProductAdded:', this.packageService.isPackage1);
     console.log('isProductAdded:', this.packageService.isPackage2);
+    console.log('isProductAdded:', this.packageService.isPackage3);
+    const user: any = {
+      username: this.authServ.username,
+      isPackage1: this.packageService.isPackage1,
+      isPackage2: this.packageService.isPackage2,
+      isPackage3: this.packageService.isPackage3
+    };
+
+    this.http.post('http://localhost:8080/update/' + user.username, user).subscribe(
+      response => console.log('State Stored', response),
+      error => console.error('Error', error)
+    );
   }
 }
